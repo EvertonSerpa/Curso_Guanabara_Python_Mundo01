150a6fea26362e283605cb8d777f8e3df0fa4459
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-20 08:34:35 -0300

    Atividade 01 (print)

---
+++ Exer_01.py
@@ -0,0 +1,8 @@
+# Exercício Python 1:Crie um programa que escreva Olá mundo na tela.
+
+print('Olá mundo!')
+
+# Comentários:
+# Usei o print para imprimir na tela uma mensagem.
+# Lembrando que tem que ter os parênteses abrindo e fechando (), pode ser usado tanto as aspas simples ''
+# como a dupla "".
