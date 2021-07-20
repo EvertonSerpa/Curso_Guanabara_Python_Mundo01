ca3858a7a8e38372940b7e5fd38d1771dfeca009
EvertonSerpa <85652736+EvertonSerpa@users.noreply.github.com>
2021-07-20 07:32:22 -0300

    Update Exer_01.py

--- Exer_01.py
+++ Exer_01.py
@@ -4,6 +4,6 @@ print('Olá mundo!!!')
 
 # Comentários:
 # Uso o símbolo (#) para fazer comentário dentro do Python.
-# Usei o comando (print) para mostrar na tela o que está escrito dentro do print,
+# Uso o comando (print) para mostrar na tela o que está escrito dentro do print,
 # lembrando que o comando tem que ser aberto e fechado por () parênteses, também tem que
 # usar aspas simples '' ou "" duplas para poder validar o comando.
