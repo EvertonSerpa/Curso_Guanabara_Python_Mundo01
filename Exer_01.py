708d33a294723d3649b3310d7c39903f2a8fa642
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-20 07:30:09 -0300

    Atividade 01 (print algo na tela.) Curso em Vídeo, professor Gustavo Guanabara.

---
+++ Exer_01.py
@@ -0,0 +1,9 @@
+# Exercício 01. Crie um programa que escreva olá mundo na tela.
+print('Olá mundo!!!')
+
+
+# Comentários:
+# Uso o símbolo (#) para fazer comentário dentro do Python.
+# Usei o comando (print) para mostrar na tela o que está escrito dentro do print,
+# lembrando que o comando tem que ser aberto e fechado por () parênteses, também tem que
+# usar aspas simples '' ou "" duplas para poder validar o comando.
