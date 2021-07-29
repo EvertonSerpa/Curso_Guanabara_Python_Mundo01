468ba985959efc8c9fba81862d043970ba328779
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-29 16:06:19 -0300

    Atividade 14 (Converso de temperatura)

---
+++ Exer_14.py
@@ -0,0 +1,10 @@
+'''Exercício Python 14: Escreva um programa que converta uma temperatura digitando
+em graus Celsius e converta para graus Fahrenheit.'''
+print()
+celsius = float(input('Informe a temperatura em Graus Celsius []'))
+fahrenheit = celsius * 1.8 + 32
+print(f'A temperatura em Celsius é {celsius}\ncom a conversão para Fahrenheit fica {fahrenheit}')
+'''Explicando o código
+Foi utulizado duas variaveis para resolução da questão.
+A primeira variavel recebe o valor em celsius informado pelo usuario.
+A segunda variavel faz a conversão das temperaturas. '''
