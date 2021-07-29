fbb2efd5bdbeb6921feac6055fd6044f5cb1aef8
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2021-07-29 16:58:42 -0300

    Atividade 16 (Porção inteira de um número)

--- Exer_16.py
+++ Exer_16.py
@@ -1,7 +1,7 @@
 '''Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e
 mostre na tela a sua porção Inteira.'''
 print()
-import math # importei a biblioteca math
+from math import trunc# importe so a fução trunc da biblioteca math
 numero = float(input('Digite um valor: ')) # variavel que recebe o número
-print(f'O valor digitado é: {numero} e sua porção inteira é: {math.trunc(numero)}')
+print(f'O valor digitado é: {numero} e sua porção inteira é: {trunc(numero)}')
 # uso a função trunc para pegar a primeira porção do número.
